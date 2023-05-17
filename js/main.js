6748f6ff4a77f955008a0ce1ef5afa4de4fcbcf3
vishal <vishalkumar18ssb786@gmail.com>
2023-05-17 09:48:04 +0530

    -

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// making the inpute range on work
+
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
+
+function handleInputChange(e) {
+    let target = e.target
+    console.log(e.target);
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    }
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    let range = document.getElementById('rangevalue')
+    range.innerHTML = (val - min) * 100 / (max - min)
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+})
+
+
+// navs & tabs from bootstrap css copying 
+window.addEventListener('load', (event) => {
+    openTab(event, 'All')
+});
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    evt.currentTarget.className += " active";
+}
+
